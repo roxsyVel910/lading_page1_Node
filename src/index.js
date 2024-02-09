b2c46471a7121c57fbe42626d20f85482cfeaea2
roxsyVq <roxsy.dj@gmail.com>
2024-02-09 08:57:56 -0500

    add configuracion server

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
  
 import express from 'express';
-import { dirname } from 'path';
+import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 import ejs from 'ejs';
 
@@ -8,7 +8,11 @@ import ejs from 'ejs';
 const app = express();
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-console.log(__dirname,'/views');
+
+
+app.set('views', join(__dirname, 'views'));
+
+
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
